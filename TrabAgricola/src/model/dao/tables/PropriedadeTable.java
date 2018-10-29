f1a2b4b1d0550c9507677f7cb5fc23d48733d696
vincent gabriel maris dafre <vinvinvinvin123@gmail.com>
2018-10-29 20:25:46 -0300

    all tables

---
+++ TrabAgricola/src/model/dao/tables/PropriedadeTable.java
@@ -0,0 +1,18 @@
+package model.dao.tables;
+
+import model.dao.base.Colum;
+import model.dao.base.Table;
+
+public class PropriedadeTable extends Table{
+    public PropriedadeTable(){
+        name="propriedade";
+        colums.add(new Colum("int","idpropriedade"));
+        colums.add(new Colum("Point","localisacao"));
+        colums.add(new Colum("String","documento"));
+        colums.add(new Colum("Date","data_cadastro"));
+        colums.add(new Colum("String","endereco"));
+
+        colums.add(new Colum("int","idcliente"));
+    }
+    
+}
