1d130b091659168c1427875fe3bbca87f4bce078
vincent gabriel maris dafre <vinvinvinvin123@gmail.com>
2018-10-29 19:56:35 -0300

    aplicado table

--- TrabAgricola/src/model/dao/tables/AplicadoTable.java
+++ TrabAgricola/src/model/dao/tables/AplicadoTable.java
@@ -1,16 +1,17 @@
 package model.dao.tables;
 
+import java.sql.Date;
+
 import model.dao.base.Colum;
 import model.dao.base.Table;
 
 public class AplicadoTable extends Table{
 	public AplicadoTable(){
-        name="Aplicado";
-        colums.add(new Colum("int","idcultura"));
-        colums.add(new Colum("String","tipo"));
-        colums.add(new Colum("String","nome"));
-        //colums.add(new Colum("String","fotos"));
-        colums.add(new Colum("String","descri"));
+
+        colums.add(new Colum("int","id"));
+        colums.add(new Colum("Date","data"));
+        colums.add(new Colum("int","idpropriedade"));
+        colums.add(new Colum("int","idtratamento"));
     }
 
 }
